20667b48360549e848be94d5ecc99c66c6c94711
ankitsingh711 <developerankit2127@gmail.com>
2026-01-05 10:49:10 +0530

    removed-gsap-scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -100,42 +100,6 @@ const RoleBasedLayoutWrapper: React.FC<{ children: ReactNode }> = ({ children })
 };
 
 function AppContent() {
-    // Smooth scrolling DISABLED per user request
-    // useEffect(() => {
-    //     const lenis = new Lenis({
-    //         duration: 0.75,
-    //         easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-    //         orientation: 'vertical',
-    //         gestureOrientation: 'vertical',
-    //         smoothWheel: true,
-    //         wheelMultiplier: 1,
-    //         smoothTouch: false,
-    //         touchMultiplier: 2,
-    //         infinite: false,
-    //     });
-
-    //     function raf(time: number) {
-    //         lenis.raf(time);
-    //         requestAnimationFrame(raf);
-    //     }
-
-    //     requestAnimationFrame(raf);
-
-    //     // Connect Lenis with GSAP ScrollTrigger
-    //     lenis.on('scroll', ScrollTrigger.update);
-
-    //     gsap.ticker.add((time) => {
-    //         lenis.raf(time * 1000);
-    //     });
-
-    //     gsap.ticker.lagSmoothing(0);
-
-    //     return () => {
-    //         lenis.destroy();
-    //     };
-    // }, []);
-
-
 
     return (
         <Router>
